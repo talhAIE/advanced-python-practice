33992a2f8aaa04677c78cf4a1d2e476f38cebe46
Talha Abbasi <talhabbasiaii@gmail.com>
2025-07-15 14:37:46 +0500

    Update till day 10

--- 01_TO_DO_List_APP.py
+++ 01_TO_DO_List_APP.py
@@ -1,50 +1,50 @@
-# To-Do List app
-from operator import itemgetter
-
-
+# TO do list
 while True:
     user_action=input("Type add, show, edit, complete or exit: ")
     user_action=user_action.strip()
 
-    match user_action:
-        case 'add':
-            todo=input("Enter a new item: ") + "\n"
-            file=open('todo.txt','r')
+    if  user_action.startswith('add'):
+        todo=user_action[4:]
+        with open('todo.txt','r') as file:
             todos=file.readlines()
-            todos.append(todo)
-            file.close()
+            todos.append(todo+'\n')
+        with open('todo.txt','w') as file:
+            file.writelines(todos)
 
-            file=open('todo.txt','w')
-            file.writelines(todos)    
-            file.close()
-
-        case 'show' | 'display':
-
-            with open('todo.txt','r') as file:
-             todos=file.readlines()   
-            for i,item in enumerate(todos):
-                print(f"{i+1}. {item}")
-            print(f"Total items: {len(todos)}")
-
-        case 'edit':
-            num=int(input("Enter the index number you want to edit: "))
+    elif user_action.startswith('show'):
+        with open('todo.txt','r') as file:
+            todos=file.readlines()
             
+        for i,item in enumerate(todos):
+            print(f"{i+1}. {item.strip('\n')}")
+        print(f"Total items: {len(todos)}")
+
+    elif user_action.startswith('edit'):
+        try:
+            num=int(user_action[5:])
             with open('todo.txt','r') as file:
                 todos=file.readlines()
                 todos[num-1]=input("Enter the new item: ") + "\n"
             with open('todo.txt','w') as file:
                 file.writelines(todos)
+        except ValueError:
+            print("Your Enter wrong input")
+            continue
 
-        case 'complete':
-            num=int(input("Enter the index number you want to complete: "))
+    elif user_action.startswith('complete'):
+        try:
+            num=int(user_action[9:])
             with open('todo.txt','r') as file:
                 todos=file.readlines()
                 complete=todos.pop(num-1)
                 print(f"{complete} completed")
             with open('todo.txt','w') as file:
                 file.writelines(todos)
-        case 'exit' | 'quit':
-            break
-    
-    # till day 6
-    
+        except IndexError:
+            print("There is no item with that number")
+    elif 'exit' in user_action or 'quit' in user_action:
+        break
+    else:
+        print("Invalid input")
+
+    # till day 10
